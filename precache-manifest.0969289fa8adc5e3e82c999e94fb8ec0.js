fe3d600d44e1278b85ace8b4b46744043f38bbbf
BuildTools <lorenzooguimaraes@gmail.com>
2020-03-20 16:08:01 -0300

    Updates

---
+++ precache-manifest.0969289fa8adc5e3e82c999e94fb8ec0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b5664037ffb25dc56870a357012e5b8",
+    "url": "/CoronaVirus/index.html"
+  },
+  {
+    "revision": "e8bf192fb434a82956df",
+    "url": "/CoronaVirus/static/js/2.7808a4c2.chunk.js"
+  },
+  {
+    "revision": "ee45fe4bc90ffb536db5566b15993c1a",
+    "url": "/CoronaVirus/static/js/2.7808a4c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6e18029d61fa97325ae",
+    "url": "/CoronaVirus/static/js/main.921f2ccc.chunk.js"
+  },
+  {
+    "revision": "9859faf6e42655c81272",
+    "url": "/CoronaVirus/static/js/runtime-main.be5a19ee.js"
+  }
+]);
